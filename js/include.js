cf0cd720f6fa8ab0e68ec17741a975d683a156c6
이지은 <tori@ryanlab.kr>
2025-07-14 13:23:25 +0900

    blog, blog-detail 경로 수정

--- js/include.js
+++ js/include.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("/html/header.html")
+    fetch("html/header.html")
         .then(res => res.text())
         .then(data => {
             document.getElementById("header").innerHTML = data;
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
             document.body.appendChild(script);
         });
 
-    fetch("/html/footer.html")
+    fetch("html/footer.html")
         .then(res => res.text())
         .then(data => {
             document.getElementById("footer").innerHTML = data;
